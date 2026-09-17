09d587db1be1f71d3dacfa2981a1d62315c4e05d
Kekoa McWilliams <kekoamcwilliams@gmail.com>
2026-09-17 14:44:57 -0700

    Add ternary operator for chapter 6 challenge 2

--- ch6-parsing-expressions/com/craftinginterpreters/lox/AstPrinter.java
+++ ch6-parsing-expressions/com/craftinginterpreters/lox/AstPrinter.java
@@ -26,6 +26,10 @@ class AstPrinter implements Expr.Visitor<String> {
   public String visitUnaryExpr(Expr.Unary expr) {
     return parenthesize(expr.operator.lexeme, expr.right);
   }
+  @Override
+  public String visitConditionalExpr(Expr.Conditional expr) {
+    return parenthesize("?:", expr.condition, expr.thenBranch, expr.elseBranch);
+  }
 
   private String parenthesize(String name, Expr... exprs) {
     StringBuilder builder = new StringBuilder();
--- ch6-parsing-expressions/com/craftinginterpreters/lox/Parser.java
+++ ch6-parsing-expressions/com/craftinginterpreters/lox/Parser.java
@@ -25,12 +25,25 @@ class Parser {
     }
  
     private Expr comma() {
-        Expr expr = equality();
+        Expr expr = conditional();
 
         while (match(COMMA)) {
             Token operator = previous();
             Expr right = equality();
             expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr conditional() {
+        Expr expr = equality();
+
+        if (match(QUESTION)) {
+            Expr thenBranch = expression();
+            consume(COLON, "Expect ':' after then branch of conditional expression.");
+            Expr elseBranch = conditional();
+            expr = new Expr.Conditional(expr, thenBranch, elseBranch);
         }
 
         return expr;
